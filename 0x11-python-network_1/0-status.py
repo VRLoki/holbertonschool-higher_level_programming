46faaa26796d9ddf57a016c8463389aada6b7999
Vincent ROESSLE <vincent.roessle@holbertonschool.com>
2021-10-18 16:42:12 +0300

    remove hbtn mentions - network #1

--- 0x11-python-network_1/0-status.py
+++ 0x11-python-network_1/0-status.py
@@ -4,7 +4,7 @@
 from urllib import request
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as page:
+    with request.urlopen("https://swapi.dev/api/") as page:
         content = page.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
